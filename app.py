07ffeed22e4476d94ff621e9772b418b1b4ebbd4
kattabhargavi006-sys <kattabhargavi006@gmail.com>
2026-06-11 14:26:42 +0530

    indenation fixed

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ st.set_page_config(
 
 st.title("AI Student Learning Assistant")
 
-question = st.text_input(
-    "Ask your question"
-)
+question = st.text_input("Ask your question")
 
 uploaded_file = st.file_uploader(
     "Upload PDF",
@@ -22,7 +20,6 @@ if st.button("Generate"):
     files = {}
 
     if uploaded_file:
-
         files["file"] = (
             uploaded_file.name,
             uploaded_file,
@@ -30,20 +27,21 @@ if st.button("Generate"):
         )
 
     response = requests.post(
-    "https://ai-student-learner-backend.onrender.com/learn",
-    data={"question": question},
-    files=files
-)
+        "https://ai-student-learner-backend.onrender.com/learn",
+        data={"question": question},
+        files=files
+    )
 
-st.write("Status Code:", response.status_code)
+    st.write("Status Code:", response.status_code)
 
-if response.status_code == 200:
-    result = response.json()
+    if response.status_code == 200:
+        result = response.json()
 
-    st.subheader("Explanation")
-    st.write(result["explanation"])
+        st.subheader("Explanation")
+        st.write(result["explanation"])
 
-    st.subheader("Quiz")
-    st.write(result["quiz"])
-else:
-    st.error(response.text)
+        st.subheader("Quiz")
+        st.write(result["quiz"])
+
+    else:
+        st.error(response.text)
